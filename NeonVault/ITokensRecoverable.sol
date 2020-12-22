0c37d65cae4daa19612d02d9cdb09f77b3123f3b
arvisingh098 <76474183+arvisingh098@users.noreply.github.com>
2020-12-23 00:10:33 +0530

    Update ITokensRecoverable.sol
    Update Token Recoverable code

--- NeonVault/ITokensRecoverable.sol
+++ NeonVault/ITokensRecoverable.sol
@@ -1,18 +1,9 @@
 // SPDX-License-Identifier: MIT!!!
 pragma solidity ^0.7.4;
 
-interface IUniswapV2Factory {
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+import "./IERC20.sol";
 
-    function feeTo() external view returns (address);
-    function feeToSetter() external view returns (address);
-
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
-    function allPairs(uint) external view returns (address pair);
-    function allPairsLength() external view returns (uint);
-
-    function createPair(address tokenA, address tokenB) external returns (address pair);
-
-    function setFeeTo(address) external;
-    function setFeeToSetter(address) external;
+interface ITokensRecoverable
+{
+    function recoverTokens(IERC20 token) external;
 }
